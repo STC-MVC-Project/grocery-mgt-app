d29da917305ffb8ec285de9f3aeeaadc3259f8eb
jeniblok <116460893+jeniblok@users.noreply.github.com>
2025-06-27 17:13:26 -0500

    Update nav.php
    updated nav.php

--- view/nav.php
+++ view/nav.php
@@ -1,4 +1,11 @@
-<nav>
-    <a href="index.php">Dashboard</a>
-    <a href="index.php?action=add">Add Grocery</a>
+<?php
+if (!isset($activePage)) {
+    $activePage = ''; 
+}
+?>
+<nav class="main-nav">
+    <ul>
+        <li><a href="index.php" class="<?= ($activePage == 'dashboard') ? 'active' : '' ?>">🏠 Dashboard</a></li>
+        <li><a href="index.php?action=add" class="<?= ($activePage == 'add') ? 'active' : '' ?>">➕ Add Grocery</a></li>
+    </ul>
 </nav>
