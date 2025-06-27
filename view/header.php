a279d96376fe419483ca0586ae14111484afe76b
jeniblok <116460893+jeniblok@users.noreply.github.com>
2025-06-27 17:12:39 -0500

    Update header.php
    updated header.php

--- view/header.php
+++ view/header.php
@@ -1,11 +1,9 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-    <title>Grocery Dashboard</title>
-    <link rel="stylesheet" href="css/style.css">
-<!DOCTYPE html>
-<html>
-<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Manage your grocery list efficiently with this grocery management system.">
     <title>Grocery Dashboard</title>
     <link rel="stylesheet" href="css/style.css">
 </head>
@@ -13,4 +11,5 @@
     <header>
         <h1>🥦 Grocery Management System</h1>
     </header>
+
     <?php include 'nav.php'; ?>
